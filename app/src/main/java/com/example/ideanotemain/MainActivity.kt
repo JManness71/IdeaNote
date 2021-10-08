b6be408ec2fa8fb15cf2733047704131573d04ed
Jarod Manness <jmmanness@student.fullsail.edu>
2021-10-08 15:02:52 -0400

    Week 2 Day 2 Replaced placeholder main page with a dynamic list.

--- app/src/main/java/com/example/ideanotemain/MainActivity.kt
+++ app/src/main/java/com/example/ideanotemain/MainActivity.kt
@@ -3,30 +3,50 @@ package com.example.ideanotemain
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val bSketch = findViewById<Button>(R.id.button_sketch)
-        val bCam = findViewById<Button>(R.id.button_camera)
-        val bNote = findViewById<Button>(R.id.button_note)
+        val listview = findViewById<ListView>(R.id.listView1)
+        val addButton = findViewById<Button>(R.id.button1)
+        val value = findViewById<EditText>(R.id.editText1)
 
-        bSketch.setOnClickListener{
-            val intent = Intent(this, PaintActivity::class.java)
-            startActivity(intent)
+        val listElements = ArrayList<String>()
+        val adapter: ArrayAdapter<String> = ArrayAdapter(this, android.R.layout.simple_list_item_1, listElements)
+
+        listview.adapter = adapter
+
+        addButton.setOnClickListener{
+            listElements.add(value.text.toString())
+            adapter.notifyDataSetChanged()
         }
 
-        bCam.setOnClickListener{
-            val intent = Intent(this, CameraActivity::class.java)
-            startActivity(intent)
-        }
-
-        bNote.setOnClickListener{
-            val intent = Intent(this, NoteActivity::class.java)
-            startActivity(intent)
-        }
+//        val bSketch = findViewById<Button>(R.id.button_sketch)
+//        val bCam = findViewById<Button>(R.id.button_camera)
+//        val bNote = findViewById<Button>(R.id.button_note)
+//
+//        bSketch.setOnClickListener{
+//            val intent = Intent(this, PaintActivity::class.java)
+//            startActivity(intent)
+//        }
+//
+//        bCam.setOnClickListener{
+//            val intent = Intent(this, CameraActivity::class.java)
+//            startActivity(intent)
+//        }
+//
+//        bNote.setOnClickListener{
+//            val intent = Intent(this, NoteActivity::class.java)
+//            startActivity(intent)
+//        }
     }
 }
